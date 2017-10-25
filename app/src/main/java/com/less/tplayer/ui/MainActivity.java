a107137bc8e1f10ce38f0c3a4255f8abf3757150
deeper <kaiyuan@qq.com>
2017-10-25 22:40:30 +0800

    update SmartProgressbar

--- app/src/main/java/com/less/tplayer/ui/MainActivity.java
+++ app/src/main/java/com/less/tplayer/ui/MainActivity.java
@@ -1,6 +1,9 @@
 package com.less.tplayer.ui;
 
-import com.less.security.Security;
+import android.os.Handler;
+import android.os.Message;
+
+import com.less.smart_progressbar.SmartProgressbar;
 import com.less.tplayer.R;
 import com.less.tplayer.base.BaseActivity;
 
@@ -8,6 +11,22 @@ import com.less.tplayer.base.BaseActivity;
  * @author Administrator
  */
 public class MainActivity extends BaseActivity {
+    private SmartProgressbar smartProgressbar;
+    private int SEND_LOADING  = 0x123;
+    private int progress = 0;
+
+    private Handler handler = new Handler(){
+        @Override
+        public void handleMessage(Message msg) {
+            progress++;
+            smartProgressbar.setProgress(progress);
+            if (progress < 100) {
+                handler.sendEmptyMessageDelayed(SEND_LOADING, 100);
+            } else {
+                handler.removeMessages(SEND_LOADING);
+            }
+        }
+    };
 
     @Override
     public int getContentView() {
@@ -17,9 +36,7 @@ public class MainActivity extends BaseActivity {
     @Override
     protected void initData() {
         super.initData();
-        Security security = new Security();
-        String jniStr = security.stringFromJNI();
-
-
+        smartProgressbar = (SmartProgressbar) findViewById(R.id.sp_progressbar);
+        handler.sendEmptyMessageDelayed(SEND_LOADING, 3000);
     }
 }
