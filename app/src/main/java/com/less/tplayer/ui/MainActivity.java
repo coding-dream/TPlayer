3859ca3dc428767f284f333ddeabf00b2a135075
deeper <kaiyuan@qq.com>
2017-10-27 11:09:31 +0800

    add widget_viewPagerEx

--- app/src/main/java/com/less/tplayer/ui/MainActivity.java
+++ app/src/main/java/com/less/tplayer/ui/MainActivity.java
@@ -1,32 +1,14 @@
 package com.less.tplayer.ui;
 
-import android.os.Handler;
-import android.os.Message;
-
-import com.less.smart_progressbar.SmartProgressbar;
 import com.less.tplayer.R;
 import com.less.tplayer.base.BaseActivity;
+import com.less.viewpager_ex.ViewPagerEx;
 
 /**
  * @author Administrator
  */
 public class MainActivity extends BaseActivity {
-    private SmartProgressbar smartProgressbar;
-    private int SEND_LOADING  = 0x123;
-    private int progress = 0;
-
-    private Handler handler = new Handler(){
-        @Override
-        public void handleMessage(Message msg) {
-            progress++;
-            smartProgressbar.setProgress(progress);
-            if (progress < 100) {
-                handler.sendEmptyMessageDelayed(SEND_LOADING, 100);
-            } else {
-                handler.removeMessages(SEND_LOADING);
-            }
-        }
-    };
+    private ViewPagerEx viewPagerEx;
 
     @Override
     public int getContentView() {
@@ -36,7 +18,11 @@ public class MainActivity extends BaseActivity {
     @Override
     protected void initData() {
         super.initData();
-        smartProgressbar = (SmartProgressbar) findViewById(R.id.sp_progressbar);
-        handler.sendEmptyMessageDelayed(SEND_LOADING, 3000);
+    }
+
+    @Override
+    protected void initView() {
+        super.initView();
+
     }
 }
