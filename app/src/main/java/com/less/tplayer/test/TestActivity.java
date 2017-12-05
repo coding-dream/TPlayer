3a0dc0beb9d9b7eed52c4047fab24f9e640b57d6
deeper <352085768@qq.com>
2017-12-05 11:37:06 +0800

    test FlowLayout

--- app/src/main/java/com/less/tplayer/test/TestActivity.java
+++ app/src/main/java/com/less/tplayer/test/TestActivity.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
 import com.less.tplayer.R;
+import com.less.uis.flowlayout.FlowLayout;
 
 /**
  * @author Administrator
@@ -15,6 +16,27 @@ public class TestActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_test);
+        FlowLayout flowLayout = (FlowLayout) findViewById(R.id.flowLayout);
+//        List<String> list = new ArrayList<>();
+//        list.add("DouYu");
+//        list.add("腾讯视频");
+//        list.add("阿里巴巴");
+//        list.add("支付宝");
+//        list.add("一行代码上你家");
+//        list.add("美女的诞生");
+//        list.add("生活大爆炸");
+//        list.add("天使的声音");
+//        list.add("爱美丽");
+//        list.add("哈哈");
+//        list.add("太好听");
+//        list.add("网易云音乐");
+//
+//        flowLayout.setFlowListener(list, new FlowLayout.OnFlowItemClickListener() {
+//            @Override
+//            public void onItemClick(String message) {
+//                Toast.makeText(TestActivity.this, message, Toast.LENGTH_SHORT).show();
+//            }
+//        });
     }
 
     public void handle(View view) {
